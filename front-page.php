aeb80e9333f77942f1add45083088fb4d837f0ea
calebmutisya <mutisyacalebmusau@gmail.com>
2025-09-16 17:21:30 +0300

    styled services

--- front-page.php
+++ front-page.php
@@ -56,14 +56,41 @@ get_header(); ?>
             </div>
         </section>
 
-        <section id="services" class="container">
-            <h2>Services</h2>
-            <p>Details about services...</p>
-        </section>
-
-        <section id="contact" class="container">
-            <h2>Contact</h2>
-            <p>Email: info@example.com</p>
+        <section id="services">
+            <div class="container py-[70px] text-center">
+                <h3 class=" font-[700] text-[42px] mb-[20px] text-[#004861]">Total Health Care Solutions</h3>
+                <p class="font-[300] text-[16px] mb-[70px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>Quisquam ut cum aperiam, doloribus praesentium a exercitationem sint quaerat delectus</p>
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center">
+                    <div class=" max-w-[260px] py-[40px] px-[20px] mb-[40px] rounded-[6px] border-none shadow-[0px_0px_10px_0px_rgba(0,0,0,0.1)]">
+                        <div class="flex justify-center">
+                            <image class="w-[64px] h-[64px] rounded-full" src="" alt=""/>
+                        </div>
+                        <h5 class="mt-[25px] mb-[15px] font-[900] text-[#004861]">MRI</h5>
+                        <p class="text-[14px] font-[300]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ut cum aperiam, doloribus praesentium a exercitationem sint quaerat delectus</p>
+                    </div>
+                    <div class=" max-w-[260px] py-[40px] px-[20px] mb-[40px] rounded-[6px] border-none shadow-[0px_0px_10px_0px_rgba(0,0,0,0.1)]">
+                        <div class="flex justify-center">
+                            <image class="w-[64px] h-[64px] rounded-full" src="" alt=""/>
+                        </div>
+                        <h5 class="mt-[25px] mb-[15px] font-[900] text-[#004861]">LABORATORY</h5>
+                        <p class="text-[14px] font-[300]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ut cum aperiam, doloribus praesentium a exercitationem sint quaerat delectus</p>
+                    </div>
+                    <div class=" max-w-[260px] py-[40px] px-[20px] mb-[40px] rounded-[6px] border-none shadow-[0px_0px_10px_0px_rgba(0,0,0,0.1)]">
+                        <div class="flex justify-center">
+                            <image class="w-[64px] h-[64px] rounded-full" src="" alt=""/>
+                        </div>
+                        <h5 class="mt-[25px] mb-[15px] font-[900] text-[#004861]">EMERGENCY HELP</h5>
+                        <p class="text-[14px] font-[300]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ut cum aperiam, doloribus praesentium a exercitationem sint quaerat delectus</p>
+                    </div>
+                    <div class=" max-w-[260px] py-[40px] px-[20px] mb-[40px] rounded-[6px] border-none shadow-[0px_0px_10px_0px_rgba(0,0,0,0.1)]">
+                        <div class="flex justify-center">
+                            <image class="w-[64px] h-[64px] rounded-full" src="" alt=""/>
+                        </div>
+                        <h5 class="mt-[25px] mb-[15px] font-[900] text-[#004861]">PEDIATRICS</h5>
+                        <p class="text-[14px] font-[300]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ut cum aperiam, doloribus praesentium a exercitationem sint quaerat delectus</p>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 </main>
